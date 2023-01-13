1546ccbcd8e6d22e2e202e2b47a252bb2e6b0506
Fiodar Sazanavets <f.sazanavets@gmail.com>
2023-01-13 09:46:16 +0000

    Add code samples from inbuilt components and templates

---
+++ BlazorProjectTemplatesExamples/InbuiltComponentsDemo/Models/Car.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InbuiltComponentsDemo.Models;
+
+public class Car
+{
+    [Required]
+    [StringLength(16, ErrorMessage = "Character limit exceeded.")]
+    public string? Make { get; set; }
+
+    [Required]
+    public string? Model { get; set; }
+
+    [Required]
+    public string? Classification { get; set; }
+
+    [Range(1, 7, ErrorMessage = "A car can have between 1 and 7 passengers.")]
+    public int MaximumPassengers { get; set; }
+
+    [Required]
+    [Range(typeof(bool), "true", "true",
+        ErrorMessage = "Unapproved designes dissallowed.")]
+    public bool IsValidatedDesign { get; set; }
+
+    [Required]
+    public DateTime ProductionDate { get; set; }
+}
