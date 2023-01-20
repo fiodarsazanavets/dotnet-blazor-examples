f4ba73ea1a09db8ff177105a316037496d8a25a7
Fiodar Sazanavets <f.sazanavets@gmail.com>
2023-01-20 13:43:56 +0800

    Add some event handling examples

---
+++ BlazorProjectTemplatesExamples/BlazorDataBinding/OnSpecialPasteHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorDataBinding;
+
+[EventHandler("onspecialpaste", typeof(SpecialPasteEventArgs),
+    enableStopPropagation: true, enablePreventDefault: true)]
+public static class EventHandlers
+{
+}
+
+public class SpecialPasteEventArgs : EventArgs
+{
+    public string? PastedData { get; set; }
+}
